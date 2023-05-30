9da2d45f0dffcf434fba41a0ede9f9940ca10c9f
Firsman <firsman1993@gmail.com>
2023-05-30 10:18:27 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        double number1 = scanner.nextDouble();
+        System.out.println("Введите операцию");
+        char operation = scanner.next().charAt(0);
+        System.out.println("Введите второе число");
+        double number2 = scanner.nextDouble();
+        double result = 0;
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            default:
+                System.out.println("Операция не поддерживается");
+        }
+        System.out.println(result);
+    }
+}
